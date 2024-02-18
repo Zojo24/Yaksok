acd9e911b03fe4a98d040176268c73029f08618c
Hong Joo Choi <hchoi2894@gmail.com>
2024-02-18 21:54:02 +0900

    Add header in main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from "../assets/logo.png";
 import Button from "../components/Button";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Home = () => {
+  const [name, setName] = useState("");
+
   return (
-    <div>
+    <div className="flex">
       <nav className="bg-darkGray flex flex-col justify-between w-14 h-screen items-center pb-2">
         <div className="flex flex-col w-full items-center">
           <img src={logo} alt="logo" className="w-10 h-13 m-2 bg-white" />
@@ -43,6 +46,28 @@ const Home = () => {
           </Button>
         </div>
       </nav>
+      <header className="flex justify-end w-full m-4">
+        <div className="mr-8">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-[20rem] h-12 bg-darkGray border border-darkGray p-2 rounded-[5px] shadow text-center text-white focus:outline-none focus:border-creme"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
+        <div className="h-12 bg-darkGray p-2 rounded-[5px] ">
+          <Button variant="creme" size="sm" className="text-[0.9rem] mx-2">
+            거리순
+          </Button>
+          <Button variant="creme" size="sm" className="text-[0.9rem] mx-2">
+            평점순
+          </Button>
+          <Button variant="creme" size="sm" className="text-[0.9rem] mx-2">
+            실시간 순위
+          </Button>
+        </div>
+      </header>
     </div>
   );
 };
