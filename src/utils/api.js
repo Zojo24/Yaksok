aea1db3dba38b2b827444f5b414c92d9a161be81
Hong Joo Choi <hchoi2894@gmail.com>
2024-02-19 15:37:27 +0900

    Add config.js file to secure Client-Id and Client-Secret

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,20 @@
 import axios from "axios";
+import { Naver_ID, Naver_Secret } from "../config";
 
 const axiosInstance = axios.create({
   baseURL: "https://openapi.naver.com",
   headers: {
-    "X-Naver-Client-Id": "wjhPXu11ak5t7jIRk9VZ",
-    "X-Naver-Client-Secret": "PP87YSUeGv",
+    "X-Naver-Client-Id": Naver_ID,
+    "X-Naver-Client-Secret": Naver_Secret,
   },
 });
 
 export const searchPlace = async (query) => {
   try {
     const response = await axiosInstance.get("/v1/search/local.json", {
-      params: { query, display: 1 },
+      params: { query, display: 20, start: 1, sort: "random" },
     });
+    console.log("네이버에서 받은 결과 =", response.data);
     return response.data.items;
   } catch (error) {
     console.error("API 호출 중 오류가 발생했습니다:", error);
